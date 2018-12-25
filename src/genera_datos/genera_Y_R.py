faba3721e56fd458614b9271c85f41318fdb2a97
raulolles <33615301+raulolles@users.noreply.github.com>
2018-12-25 10:47:12 +0100

    #6
    scraping y generación de tablas Y y R

---
+++ src/genera_datos/genera_Y_R.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Genera las Tablas Y R a partir de:
+    - cantidad de juegos
+    - 1000 usuarios ficticios con 100 valoraciones aleatorias por usuario
+
+Matriz Y: ratings
+Matriz X: juegos valorados
+"""
+
+import numpy as np
+import random
+
+
+ratingMax = 5
+nUsers = 1000
+nValoraciones = 100
+
+
+# Genera valoraciones aleatorias entre 0 y 5
+def generaTablas(nJuegos):
+    
+    # Crea Y R a zeros
+    Y = np.zeros ((nJuegos, nUsers), dtype = np.float)
+    R = np.zeros ((nJuegos, nUsers), dtype = np.int)
+    
+    # Rellena Y
+    for user in range(nUsers):
+        for j in range(nValoraciones):
+            Y[random.randint(0,nJuegos-1), user] = random.randint(0, ratingMax)
+
+    # Genera R
+    R[Y>0]=1
+    
+    # Guarda Matrices
+    np.save('../datos/Y', Y)
+    np.save('../datos/R', R)
+   
+    
+    
---
+++ src/genera_datos/scraping.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 22 08:05:00 2018
+
+@author: Usuario
+"""
+
+from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+from time import time
+from genera_datos import genera_Y_R as g
+
+driver = webdriver.Chrome()
+
+continua = True
+pagina = 1
+pag = 0
+nJuegos = 0
+tiempo = time()
+csvfile = open('../datos/datosJuegos.csv', 'w')
+
+while continua:
+
+    webSite = "https://archive.org/details/softwarelibrary_c64_games?&sort=-downloads&page=" + str(pagina)
+    try:
+        driver.get(webSite)
+
+        # En elem: Nombre_Juego, Empresa, eye, favorite, comment
+        elem = driver.find_elements_by_xpath("//div [@class='item-ia hov']")
+        
+        # En elem2 Imagenes Links
+        elem2 = driver.find_elements_by_class_name("item-img ")        
+        
+        if len(elem) > 0:
+            nJuegos = nJuegos +  len(elem)
+            pag = pag+1
+            pagina = pagina +1
+            
+            i=0
+            while i < len(elem):
+                juego = elem[i].text.split('\n')
+                long= len(juego)
+                juego[1] = juego[1][3:]
+                
+                link = elem2[i].get_attribute('src').split('/')
+                linkOK = link[len(link)-1]
+                
+                csvfile.write(juego[0]+";"+juego[1]+";"+juego[long-5]+";"+juego[long-3]+";"+juego[long-1]+";"+linkOK+"\n")
+                i = i+1                
+        else: 
+            continua = False
+                
+        print("   --> Paginas Analizadas   : ", pag)
+        print("   --> Juegos Referenciados : ", nJuegos)    
+        
+    except:
+        continua = False
+        
+        
+
+csvfile.close()
+driver.close()
+
+
+# Genera tablas Y R
+#   Evitamos utilziar en futuro datos desfasados para filtros
+g.generaTablas(nJuegos)
+
+print("\n-------  Resultados del Scraping --------")
+print("   --> Paginas Analizadas   : ", pag)
+print("   --> Juegos Referenciados : ", nJuegos)
+print("   --> Tiempo               : %.2f segundos" %(time()-tiempo))
+
+# 17.000  = 134 páginas
+# /services/img/Castle_Wolfenstein_1983_Muse_Software
+#/details/Castle_Wolfenstein_1983_Muse_Software
+        
+# Opción todas juntas
+#elem = driver.find_element_by_id("ikind--downloads")
