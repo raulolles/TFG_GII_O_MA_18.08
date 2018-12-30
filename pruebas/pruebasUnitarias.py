5c97b1c39c3486c28174fa8bb2c2ad0e69b419ea
raulolles <33615301+raulolles@users.noreply.github.com>
2018-12-30 07:14:24 +0100

    #13
    Pruebas Unitarias Filtros
    - Modelos
    - Memoria Usuarios
    - Memoria Juegos

--- pruebas/pruebasUnitarias.py
+++ pruebas/pruebasUnitarias.py
@@ -46,5 +46,47 @@ class PruebasUnitarias(unittest.TestCase):
             print ("       I/O error({0}): {1}".format(e.errno, e.strerror))
             
         
+    def test_P_Modelos (self):
+        try:
+            P_Modelos = np.load('..\src\datos\P_Modelos.npy')
+            nJuegos, nUsers = P_Modelos.shape
+            P2 = np.zeros ((nJuegos, nUsers), dtype = np.int)
+            P2[P_Modelos == 0] = 1
+            self.assertEqual (P2.all(),0)
+            P2[P_Modelos > 5] = 1
+            self.assertEqual (P2.all(),0)
+        except IOError as e:
+            print("\n --> Error en test_P:  Verifique que existe la matriz P_Modelos")
+            print ("       I/O error({0}): {1}".format(e.errno, e.strerror))
+
+
+    def test_P_Mem_Users (self):
+        try:
+            P_Mem_Users = np.load('..\src\datos\P_Mem_Users.npy')
+            nJuegos, nUsers = P_Mem_Users.shape
+            P2 = np.zeros ((nJuegos, nUsers), dtype = np.int)
+            P2[P_Mem_Users == 0] = 1
+            self.assertEqual (P2.all(),0)
+            P2[P_Mem_Users > 5] = 1
+            self.assertEqual (P2.all(),0)
+        except IOError as e:
+            print("\n --> Error en test_P:  Verifique que existe la matriz P_Mem_Users")
+            print ("       I/O error({0}): {1}".format(e.errno, e.strerror))            
+
+
+    def test_P_Mem_Juegos (self):
+        try:
+            P_Mem_Juegos = np.load('..\src\datos\P_Mem_Juegos.npy')
+            nJuegos, nUsers = P_Mem_Juegos.shape
+            P2 = np.zeros ((nJuegos, nUsers), dtype = np.int)
+            P2[P_Mem_Juegos == 0] = 1
+            self.assertEqual (P2.all(),0)
+            P2[P_Mem_Juegos > 5] = 1
+            self.assertEqual (P2.all(),0)
+        except IOError as e:
+            print("\n --> Error en test_P:  Verifique que existe la matriz P_Mem_Users")
+            print ("       I/O error({0}): {1}".format(e.errno, e.strerror)) 
+
+
 if __name__ == '__main__':
     unittest.main()
