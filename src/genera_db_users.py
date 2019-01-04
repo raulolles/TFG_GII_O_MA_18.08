f3b1bf78f34bd3f78cb4e07dc17d45a1c95b57a1
raulolles <33615301+raulolles@users.noreply.github.com>
2019-01-04 05:18:22 +0100

    #17
    Base de datos Usuarios
    Archivos de configuración

---
+++ src/genera_db_users.py
@@ -0,0 +1,15 @@
+from app import db
+from app.models import User
+import numpy as np
+
+for i in range(0,1000):
+	name = 'user'+str(i)
+	mail = name+'@retrogaming.com'
+	avat = 'img'+str(np.random.randint(1,31))
+	pssw = 'parola'
+	
+	user = User(username=name, email=mail, avatar=avat)
+	user.set_password(pssw)
+	db.session.add(user)
+
+db.session.commit()
