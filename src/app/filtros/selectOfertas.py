ce01523051fb0bee36710a6f4cf9a50fe5c708cd
raulolles <33615301+raulolles@users.noreply.github.com>
2019-01-14 12:37:29 +0100

    #28
    Actualiza las funciones de selección de juegos para retornar datos estadísticos.
    
    Crea una nueva función para devolver los juegos favoritos

--- src/app/filtros/selectOfertas.py
+++ src/app/filtros/selectOfertas.py
@@ -2,13 +2,65 @@ import numpy as np
 from pandas import read_table
 import random
 
+def calcula_estad_juego(juego,y,r):
+	tabla_estad = list()
+	
+	y_juego = y[juego,:]
+	users_jugado = np.sum(r[juego,:])
+	puntuacion = np.sum(y_juego)
+	puntuacion_media = puntuacion/users_jugado
+	imagen_punt = imagen_puntuacion(puntuacion_media)
+	
+	tabla_estad.append(juego)
+	tabla_estad.append(str("{:.1f}".format(puntuacion_media)))
+	tabla_estad.append(imagen_punt)
+	tabla_estad.append(users_jugado)
+	
+	y_jugados = y[juego,:]
+	
+	tabla_estad.append(len(np.where(y_juego ==5)[0]))
+	tabla_estad.append(len(np.where(y_juego ==4)[0]))
+	tabla_estad.append(len(np.where(y_juego ==3)[0]))
+	tabla_estad.append(len(np.where(y_juego ==2)[0]))
+	tabla_estad.append(len(np.where(y_juego ==1)[0]))
 
-def select_de_matriz(p,r,id_user,unid_select,items):
+	return tabla_estad
+	
+
+def imagen_puntuacion(puntuacion):
+	if puntuacion > -0.5 and puntuacion <= 0.5:
+		str_imagen = "simbolos/star_cero.png"
+	elif puntuacion < 1:
+		str_imagen = "simbolos/star_cero_med.png"	
+	elif puntuacion < 1.5:
+		str_imagen = "simbolos/star_uno.png"	
+	elif puntuacion < 2:
+		str_imagen = "simbolos/star_uno_med.png"	
+	elif puntuacion < 2.5:
+		str_imagen = "simbolos/star_dos.png"
+	elif puntuacion < 3:
+		str_imagen = "simbolos/star_dos_med.png"
+	elif puntuacion < 3.5:
+		str_imagen = "simbolos/star_tres.png"
+	elif puntuacion < 4:
+		str_imagen = "simbolos/star_tres_med.png"
+	elif puntuacion < 4.5:
+		str_imagen = "simbolos/star_cuatro.png"
+	elif puntuacion < 5:
+		str_imagen = "simbolos/star_cuatro_med.png"
+	else:
+		str_imagen = "simbolos/star_cinco.png"
+	
+	return str_imagen
+
+	
+def select_de_matriz(y,p,r,id_user,unid_select,items):
+	r0 = r[:,id_user]
 	# Crea una matriz en la que se guardan los indices, R0 y P0
 	p0= p[:,id_user]
-	ind = range(0, len(r))
-	l = np.concatenate((ind, r, p0))
-	l = l.reshape(3, len(r))
+	ind = range(0, len(r0))
+	l = np.concatenate((ind, r0, p0))
+	l = l.reshape(3, len(r0))
 	l = l.T
 
 	# Selecciona valores y Ordena la matriz
@@ -16,12 +68,22 @@ def select_de_matriz(p,r,id_user,unid_select,items):
 	l = l[l[:,2].argsort()]       # ordena la matriz por P
 	l = np.flipud(l)              # voltea la matriz para ofrecer en orden correcto
 
-	seleccion = items.loc[l[:,0][0:unid_select]].values.tolist()
+	seleccion = list()
+	linea_select_a = list()
+	linea_select_b = list()
+	
+	for i in range (unid_select):
+		juego = int(l[i][0])
+		print (l)
+		print(juego)
+		linea_select_a = items.loc[juego].values.tolist()
+		linea_select_b = calcula_estad_juego(juego,y,r)
+		seleccion.append(linea_select_a + linea_select_b)
+		
 	return seleccion
 	
 	
 def importa_tablas():
-
 	# Importa Matrices
 	y = np.load('app\static\datos\Y.npy')
 	r = np.load('app\static\datos\R.npy')
@@ -36,14 +98,14 @@ def importa_tablas():
 	return y, r, p_modelos, p_mem_users, p_mem_juegos, items
 	
 	
-def select(id_user):
+def select_predicciones(id_user):
 	unid_select = 5
 	y, r, p_modelos, p_mem_users, p_mem_juegos, items = importa_tablas()
 	r0 = r[:,id_user]
 
-	select_users = select_de_matriz(p_mem_users,r0,id_user,unid_select,items)	
-	select_juegos = select_de_matriz(p_mem_juegos,r0,id_user,unid_select,items)
-	select_modelos = select_de_matriz(p_modelos,r0,id_user,unid_select,items)
+	select_users = select_de_matriz(y,p_mem_users,r,id_user,unid_select,items)	
+	select_juegos = select_de_matriz(y,p_mem_juegos,r,id_user,unid_select,items)
+	select_modelos = select_de_matriz(y,p_modelos,r,id_user,unid_select,items)
 
 	return select_modelos, select_users, select_juegos
 	
@@ -52,9 +114,45 @@ def select_aleatorio():
 	y, r, p_modelos, p_mem_users, p_mem_juegos, items = importa_tablas()
 	n_juegos,n_users = y.shape
 	n_juegos = n_juegos-1
-	select = list()
+	seleccion = list()
+	linea_select_a = list()
+	linea_select_b = list()
 	
 	for i in range (unid_select):
-		select.append(items.loc[random.randint(0,n_juegos)].values.tolist())
+		juego = random.randint(0,n_juegos)
+		linea_select_a = items.loc[juego].values.tolist()
+		linea_select_b = calcula_estad_juego(juego,y,r)
+		seleccion.append(linea_select_a + linea_select_b)
+		
+	return seleccion
+
+def select_favoritos(id_user):
+	unid_select = 5
+	y, r, p_modelos, p_mem_users, p_mem_juegos, items = importa_tablas()
+	r0 = r[:,id_user]
 	
-	return select
+	# Crea una matriz en la que se guardan los indices, R0 y P0
+	y0= y[:,id_user]
+	ind = range(0, len(r0))
+	l = np.concatenate((ind, r0, y0))
+	l = l.reshape(3, len(r0))
+	l = l.T
+
+	# Selecciona valores y Ordena la matriz
+	l = l[np.where(l[:,1] == 1)]  # deja en la matriz los juegos sí jugados
+	l = l[l[:,2].argsort()]       # ordena la matriz por Y
+	l = np.flipud(l)              # voltea la matriz para ofrecer en orden correcto
+
+	seleccion = list()
+	linea_select_a = list()
+	linea_select_b = list()
+	
+	for i in range (unid_select):
+		juego = int(l[i][0])
+		print (l)
+		print(juego)
+		linea_select_a = items.loc[juego].values.tolist()
+		linea_select_b = calcula_estad_juego(juego,y,r)
+		seleccion.append(linea_select_a + linea_select_b)
+		
+	return seleccion
