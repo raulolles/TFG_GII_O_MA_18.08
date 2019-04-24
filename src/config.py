0539aac4bba11724f7a965e8da3e5c55e5ee83b9
raulolles <33615301+raulolles@users.noreply.github.com>
2019-04-24 06:07:26 +0200

    #54
    Modificación y creación de archivos para hosting en Heroku

--- src/config.py
+++ src/config.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SEL_TOTAL = 100
-	SEL_POR_PAG = 6
+	SEL_TOTAL = 15
+	SEL_POR_PAG = 4
